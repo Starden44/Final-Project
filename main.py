ece32d1708fe37152614320281681778dbdbe021
starden44 <starden44@gmail.com>
2026-07-30 09:22:05 -0500

    Finished-implementing-gui

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-"Hello World"
+from logic import *
+
+def main():
+    application = QApplication([])
+    window = Logic()
+    window.show()
+    application.exec()
+
+if __name__ == "__main__":
+    main()
